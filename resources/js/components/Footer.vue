6bc63111431c71c79069d78ef0521595b43ab8b2
JuJu222 <japjustin@yahoo.com>
2022-05-17 21:16:00 +0700

    Changed template text

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -22,7 +22,7 @@
 </ul>
 </div>
 <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8">
-<span class="block text-sm text-gray-500 sm:text-center font-opensans dark:text-gray-400">© 2022 <a href="https://flowbite.com" class="hover:underline">Nyehaat.id</a>. Surabaya,Indonesia
+<span class="block text-sm text-gray-500 sm:text-center font-opensans dark:text-gray-400">© 2022 <a href="/" class="hover:underline">Nyehaat.id</a>. Surabaya,Indonesia
 </span>
 </footer>
 
